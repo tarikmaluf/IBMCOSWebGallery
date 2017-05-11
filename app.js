c7a2e3fdd867c18d133cf9b8c3c83e4976ae3428
tarikmaluf <tmaluf@tmaluf.local>
2017-05-11 19:14:52 -0300

    deploy

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-/*eslint-env node*/
-
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
-// create a new express server
+var bodyParser = require('body-parser');
 var app = express();
 
-// serve static files out of ./public
-app.use(express.static(__dirname + '/public'));
+// serve the files out of ./public as our main files
+app.use(express.static('public'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 
-// serve the initial view
+var title = 'Simple COS Web Gallery';
+// Serve index.ejs
 app.get('/', function (req, res) {
-  res.sendFile(__dirname +'/src/views/index.html');
+  res.render('index', {status: '', title: title});
 });
+
+var imageUploadRouter = require('./src/routes/imageUploadRoutes')(title);
+var galleryRouter = require('./src/routes/galleryRoutes')(title);
+
+app.use('/gallery', galleryRouter);
+app.use('/', imageUploadRouter);
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-var port = process.env.PORT || 3000
+// start server on the specified port and binding host
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
